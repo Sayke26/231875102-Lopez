67d33e1e89134fae23c106f6857f85caa18f5395
S312User <S312User@S312.uc-bcf.edu.ph>
2025-03-12 14:43:05 +0800

    added PHP file

---
+++ PHP Activities/Sayk.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Exercise 1 :"
+    $a = 15;
+    $b = 7;
+    $c = $a + $b;
+    $d = $a - $b;
+    $e = $a * $b;
+    $f = $a / $b;
+    echo "The Sum is: " . $c;
+    echo "The Difference is: " . $d;
+    echo "The Product is: " . $e;
+    echo "The Quotient is: " . $f;
+    ?>
+</body>
+</html>
